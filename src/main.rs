cd262404967bd4e63a8d955b495eb4f152be71df
Florian Jung <flo@windfis.ch>
2022-04-02 18:43:57 +0200

    Stability and rationality

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,21 @@ use rand::random;
 use std::fs::File;
 use std::io::Write;
 
-const STEPS: usize = 10_000_000;
+const STEPS: usize = 100_000_000;
 const SUBSTEPS: usize = 10;
 const N_ACTORS: usize = 1000;
 
 struct Traits {
-	egoism: f64
+	/// How much of an asshole
+	egoism: f64,
+	/// How much happiness contributes to the effective egoism
+	rationality: f64,
+	/// How fast happiness changes
+	stability: f64
 }
 
 struct Mindstate {
-	happiness: f64
+	happiness: f64,
 }
 
 struct Actor {
@@ -23,6 +28,16 @@ struct Actor {
 trait SliceExt {
 	type Output;
 	fn get_mut2(&mut self, a: usize, b: usize) -> (&mut Self::Output, &mut Self::Output);
+}
+
+impl Actor {
+	pub fn nudge_happiness(&mut self, target: f64) {
+		self.mindstate.happiness = (1.0 - self.traits.stability) * target + self.traits.stability * self.mindstate.happiness;
+	}
+
+	pub fn effective_egoism(&mut self) -> f64 {
+		self.traits.rationality * self.traits.egoism + (1.0 - self.traits.rationality) * (1.0 - self.mindstate.happiness)
+	}
 }
 
 impl<T> SliceExt for [T] {
@@ -52,15 +67,19 @@ fn play_n(a1: &mut Actor, a2: &mut Actor, n: usize) {
 
 fn play_one(a1: &mut Actor, a2: &mut Actor) {
 	for _ in 0..SUBSTEPS {
-		if a1.mindstate.happiness > a1.traits.egoism {
+		if a1.effective_egoism() < 0.5 {
 			a1.money -= 1;
 			a2.money += 2;
-			a2.mindstate.happiness = (1.0 + a2.mindstate.happiness) / 2.0;
+			a2.nudge_happiness(1.0);
 		}
 		else {
-			a2.mindstate.happiness = (0.0 + a2.mindstate.happiness) / 2.0;
+			a2.nudge_happiness(0.0);
 		}
 	}
+}
+
+fn avg_happiness(actors: &[Actor]) -> f64 {
+	actors.iter().map(|a| a.mindstate.happiness).sum::<f64>() / (actors.len() as f64)
 }
 
 fn game(actors: &mut [Actor]) {
@@ -76,11 +95,21 @@ fn game(actors: &mut [Actor]) {
 	}
 }
 
+fn bar(x: f64) -> String {
+	let len = 40;
+	let first = (len as f64 * x) as usize;
+	format!("{}{} {:5.3}", "#".repeat(first), " ".repeat(len-first), x)
+}
+
 fn main() {
 	let mut actors: Vec<Actor> = (0..N_ACTORS).map(
 		|_| Actor {
 			money: 100,
-			traits: Traits { egoism: random() },
+			traits: Traits {
+				egoism: random(),
+				rationality: 0.5,
+				stability: 0.5
+			},
 			mindstate: Mindstate { happiness: 0.5 }
 		}
 	).collect();
@@ -91,14 +120,21 @@ fn main() {
 	for i in 0..STEPS {
 		game(&mut actors);
 
-		if i % 10000 == 0 {
-			eprintln!("Progress: {} / {}", i, STEPS);
+		if i % 100000 == 0 {
+			eprintln!("Progress: {} / {}\t\tHappiness: {} 1% / mid / 99% = {:5.3} {:e} {:5.3}",
+				i,
+				STEPS,
+				&bar(avg_happiness(&actors)),
+				actors.iter().filter(|a| a.mindstate.happiness < 0.01).count() as f64 / actors.len() as f64,
+				actors.iter().filter(|a| (0.01 .. 0.99).contains(&a.mindstate.happiness)).count() as f64 / actors.len() as f64,
+				actors.iter().filter(|a| a.mindstate.happiness > 0.99).count() as f64 / actors.len() as f64,
+				);
 		}
 
-		if i % 1000 == 0 {
+		if i % 10000 == 0 {
 			writeln!(&mut happiness_file, "{:20} {:20}",
 				i,
-				actors.iter().map(|a| a.mindstate.happiness).sum::<f64>() / (actors.len() as f64)
+				avg_happiness(&actors)
 			).unwrap();
 		}
 	}
